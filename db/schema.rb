6542def9bc68f19568b1f03fe7ad710184238957
at0misk <ktp925@gmail.com>
2017-08-18 13:32:34 -0700

    email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811152127) do
+ActiveRecord::Schema.define(version: 20170818203109) do
+
+  create_table "emails", force: :cascade do |t|
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first"
